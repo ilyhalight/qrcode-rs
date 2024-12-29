1e58c5783d7d03b70841914604f8722afe01673c
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-12-29 19:07:56 +0300

    feat: add qrcode str and small improve error handling

--- pkg/index.ts
+++ pkg/index.ts
@@ -26,14 +26,74 @@ const lib = dlopen(libPath, {
     args: [FFIType.cstring],
     returns: FFIType.cstring,
   },
+  qrcode_str: {
+    args: [FFIType.cstring],
+    returns: FFIType.cstring,
+  },
 });
 
 const enc = new TextEncoder();
 
-export function qrcode(text: string) {
+export enum QRCodeError {
+  /**
+   * The data is too long to encode into a QR code for the given version
+   */
+  DataTooLong = "data too long",
+  /**
+   * The provided version / error correction level combination is invalid
+   */
+  InvalidVersion = "invalid version",
+  /**
+   * Some characters in the data cannot be supported by the provided QR code version
+   */
+  UnsupportedCharacterSet = "unsupported character set",
+  /**
+   * The provided ECI designator is invalid. A valid designator should be between 0 and 999999.
+   */
+  InvalidEciDesignator = "invalid ECI designator",
+  /**
+   * A character not belonging to the character set is found.
+   */
+  InvalidCharacter = "invalid character",
+
+  /**
+   * Custom error with invalid utf8 text
+   */
+  InvalidUtf8Sequence = "invalid UTF-8 sequence",
+}
+
+function throwOnError(resultStr: string) {
+  if (Object.values(QRCodeError).includes(resultStr as QRCodeError)) {
+    throw new Error(resultStr);
+  }
+
+  if (resultStr.includes("invalid utf-8 sequence")) {
+    throw new Error(QRCodeError.InvalidUtf8Sequence);
+  }
+
+  return true;
+}
+
+export function qrcode(text: string): null | string {
   if (!text) {
     return null;
   }
 
-  return lib.symbols.qrcode(enc.encode(`${text}\0`));
+  const result = lib.symbols.qrcode(enc.encode(`${text}\0`));
+  const resultStr = result.toString();
+  throwOnError(resultStr);
+
+  return `data:image/png;base64,${resultStr}`;
+}
+
+export function qrcodeString(text: string) {
+  if (!text) {
+    return null;
+  }
+
+  const result = lib.symbols.qrcode_str(enc.encode(`${text}\0`));
+  const resultStr = result.toString();
+  throwOnError(resultStr);
+
+  return resultStr;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,30 +9,62 @@ use std::{
     io::Cursor,
 };
 
-#[no_mangle]
-pub extern "C" fn qrcode(ptr: *const c_char) -> *const c_char {
+fn qrcode_internal(ptr: *const c_char) -> Result<QrCode, String> {
     let text_str = unsafe { CStr::from_ptr(ptr) };
     let text = match text_str.to_str() {
         Ok(s) => s,
-        Err(e) => {
-            eprintln!("Bad encoding: {}", e);
-            return std::ptr::null();
+        Err(err) => {
+            return Err(format!("{err}"));
         }
     };
 
-    let code = QrCode::new(text.as_bytes()).unwrap();
+    return match QrCode::new(text.as_bytes()) {
+        Ok(qrcode) => Ok(qrcode),
+        Err(err) => Err(format!("{err}")),
+    };
+}
+
+fn return_c_str(text: String) -> *const c_char {
+    let c_str = CString::new(text).unwrap();
+    let p = c_str.as_ptr();
+    std::mem::forget(c_str);
+    p
+}
+
+#[no_mangle]
+pub extern "C" fn qrcode(ptr: *const c_char) -> *const c_char {
+    let qrcode = match qrcode_internal(ptr) {
+        Ok(qrcode) => qrcode,
+        Err(err) => {
+            return return_c_str(err);
+        }
+    };
 
     // Render the bits into an image.
-    let image = code.render::<Luma<u8>>().build();
+    let image = qrcode.render::<Luma<u8>>().build();
 
     let mut image_data: Vec<u8> = Vec::new();
     image
         .write_to(&mut Cursor::new(&mut image_data), ImageFormat::Png)
         .unwrap();
     let res_base64 = general_purpose::STANDARD.encode(image_data);
-    let result = format!("data:image/png;base64,{}", res_base64);
-    let c_str = CString::new(result).unwrap();
-    let p = c_str.as_ptr();
-    std::mem::forget(c_str);
-    p
+    return_c_str(res_base64)
+}
+
+#[no_mangle]
+pub extern "C" fn qrcode_str(ptr: *const c_char) -> *const c_char {
+    let qrcode = match qrcode_internal(ptr) {
+        Ok(qrcode) => qrcode,
+        Err(err) => {
+            return return_c_str(err);
+        }
+    };
+
+    let qrcode_str = qrcode
+        .render::<char>()
+        .dark_color('#')
+        .quiet_zone(false)
+        .module_dimensions(2, 1)
+        .build();
+    return_c_str(qrcode_str)
 }
